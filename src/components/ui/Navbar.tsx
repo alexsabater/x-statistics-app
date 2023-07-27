d727c89c57f2f90009127dc714c4bd91570559cd
JaviParMu <mrjavistinson@gmail.com>
2023-07-27 19:41:38 +0200

    eslint config

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,17 +1,17 @@
-import Image from "next/image"
-import Link from "next/link"
-import { FC } from "react"
+import Image from 'next/image'
+import Link from 'next/link'
+import { FC } from 'react'
 
 const Navbar: FC = () => {
   return (
     <nav>
       <div className="nav-wrapper flex flex-row align-center py-5 px-32">
         <Link href="/" className="brand-logo">
-          <img src="/images/logo.png" alt="logo" width={50} height={50} />
+          <Image src="/images/logo.png" alt="logo" width={50} height={50} />
         </Link>
         <ul className="flex flex-row align-center justify-center ml-10">
-            <li><Link href="/">Todo List</Link></li>
-            <li><Link href="/profile">Profile</Link></li>
+          <li><Link href="/">Todo List</Link></li>
+          <li><Link href="/profile">Profile</Link></li>
         </ul>
       </div>
     </nav>
