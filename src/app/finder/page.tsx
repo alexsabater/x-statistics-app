ce7af77443de8f87da5ccdc474e8e888b6db4f56
JaviParMu <mrjavistinson@gmail.com>
2023-07-28 17:16:59 +0200

    small fixes

--- src/app/finder/page.tsx
+++ src/app/finder/page.tsx
@@ -1,13 +1,13 @@
 const FinderPage = () => {
   return (
     <div className="mt-20">
-      <head className="flex flex-col items-center justify-center">
+      <header className="flex flex-col items-center justify-center">
         <h1 className='bg-gradient-to-r bg-clip-text text-transparent from-blue-500 via-white-500 to-pink-500
             animate-text text-7xl font-bold text-center py-10'>
           <span className="text-[rgb(var(--primary-color))]">Discover</span> 
           <span className="text-[rgb(var(--primary-color))]"> everything about X.</span>
         </h1>
-      </head>
+      </header>
     </div>
   )
 }
